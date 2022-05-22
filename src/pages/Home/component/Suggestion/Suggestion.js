48966bc06a560b81f928109a3f69e3af8d7a0499
anuj-kumary <anujf0510@gmail.com>
2022-05-22 17:28:11 +0530

    feat : Sort Post By Trending and Created by

--- src/pages/Home/component/Suggestion/Suggestion.js
+++ src/pages/Home/component/Suggestion/Suggestion.js
@@ -1,6 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Box, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material';
+import Tab from '@mui/material/Tab';
+import { sortByTrendingAndLatest } from '../../../../store/postSlice';
 import { getAllUser } from '../../../../store/userSlice';
 import { SuggestionCard } from './SuggestionCard';
 
@@ -8,6 +19,8 @@ export const Suggestion = () => {
   const dispatch = useDispatch();
   const { users } = useSelector((state) => state.users);
   const { user } = useSelector((state) => state.auth);
+  const [sort, setSort] = useState(true);
+  const { posts } = useSelector((state) => state.posts);
 
   useEffect(() => {
     dispatch(getAllUser());
@@ -19,21 +32,40 @@ export const Suggestion = () => {
       currUser.username !== user.username,
   );
 
+  const sortByHandler = (e) => {
+    setSort((sort) => !sort);
+    console.log(e.target.innerText);
+    dispatch(sortByTrendingAndLatest(e.target.innerText));
+  };
+
   return (
     <>
-      <Box
-        sx={{
-          padding: '1rem',
-          backgroundColor: 'white',
-          borderRadius: '5px',
-        }}>
-        <Typography sx={{ paddingBottom: '1rem', fontWeight: '650' }} variant='p' component='p'>
-          Suggestions for you
-        </Typography>
-        {userSuggestion.map(
-          (users) =>
-            users.username !== user.username && <SuggestionCard key={users._id} users={users} />,
-        )}
+      <Box sx={{ display: 'flex', flexDirection: 'column', margin: '2rem' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', paddingBottom: '1rem', gap: '1rem' }}>
+          <Button
+            onClick={(e) => dispatch(sortByTrendingAndLatest(e.target.innerText))}
+            variant='outlined'>
+            Trending
+          </Button>
+          <Button onClick={(e) => sortByHandler(e)} variant='outlined'>
+            {sort ? 'OLDEST' : 'LATEST'}
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            padding: '1rem',
+            backgroundColor: 'white',
+            borderRadius: '5px',
+            marginRight: '1rem',
+          }}>
+          <Typography sx={{ paddingBottom: '1rem', fontWeight: '650' }} variant='p' component='p'>
+            Suggestions for you
+          </Typography>
+          {userSuggestion.map(
+            (users) =>
+              users.username !== user.username && <SuggestionCard key={users._id} users={users} />,
+          )}
+        </Box>
       </Box>
     </>
   );
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,34 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import LogoutIcon from '@mui/icons-material/Logout';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import SaveIcon from '@mui/icons-material/Save';
-import ShareIcon from '@mui/icons-material/Share';
 import {
   Avatar,
   Box,
   Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
   Container,
   CssBaseline,
-  IconButton,
   Link,
   Paper,
   Typography,
 } from '@mui/material';
-import { red } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { getUserPosts } from '../../store/profileSlice';
 import { followUser, unfollowUser } from '../../store/userSlice';
 import { PostFeed } from '../Home/component/PostFeed/PostFeed';
+import { Suggestion } from '../Home/component/Suggestion/Suggestion';
 import { EditProfile } from './components/EditProfile';
 import { FollowersModal } from './components/FollowersModal';
 import { FollowingModal } from './components/FollowingModal';
@@ -37,7 +24,6 @@ const theme = createTheme();
 
 export const Profile = () => {
   const { username } = useParams();
-  const { profileDetails } = useSelector((state) => state.profile);
   const { postDetails } = useSelector((state) => state.profile);
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
@@ -76,91 +62,107 @@ export const Profile = () => {
   return (
     <>
       {currentUserDetails && (
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <EditProfile handleClose={handleClose} open={open} />
-          <FollowingModal
-            openFollowingModal={openFollowingModal}
-            handleCloseFollowingModal={handleCloseFollowingModal}
-            following={currentUserDetails?.following}
-          />
-          <FollowersModal
-            handleCloseFollowersModal={handleCloseFollowersModal}
-            openFollowersModal={openFollowersModal}
-            followers={currentUserDetails?.followers}
-          />
+        <Box sx={{ display: 'flex' }}>
+          <Container
+            sx={{
+              width: 'auto',
+              margin: '1rem auto 0',
+              display: 'flex',
+              padding: '0 1rem',
+              flexDirection: 'column',
+            }}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <EditProfile handleClose={handleClose} open={open} />
+              <FollowingModal
+                openFollowingModal={openFollowingModal}
+                handleCloseFollowingModal={handleCloseFollowingModal}
+                following={currentUserDetails?.following}
+              />
+              <FollowersModal
+                handleCloseFollowersModal={handleCloseFollowersModal}
+                openFollowersModal={openFollowersModal}
+                followers={currentUserDetails?.followers}
+              />
 
-          <Container component='main' maxWidth='sm' sx={{ mb: 4 }}>
-            <Paper variant='outlined' sx={{ marginTop: '1rem', p: { xs: 2, md: 3 } }}>
-              <Box sx={{ display: 'flex', justifyContent: ' space-around' }}>
-                <Avatar
-                  alt='Remy Sharp'
-                  src={currentUserDetails?.avatar}
-                  sx={{ width: 100, height: 100 }}
-                />
-                <Box>
-                  <Typography
-                    sx={{ display: 'inline-block', padding: '1rem 1rem 0 1rem' }}
-                    variant='h5'
-                    component='h5'>
-                    {currentUserDetails?.firstName} {currentUserDetails?.lastName}
-                  </Typography>
-                  {user?.username === currentUserDetails?.username && (
-                    <Button onClick={handleOpen} sx={{ marginLeft: '8rem' }} variant='contained'>
-                      Edit
-                    </Button>
-                  )}
-                  {user.username !== currentUserDetails.username && (
-                    <Button variant='contained' h='2rem' onClick={unFollowOrFollowHandler}>
-                      {isFollowed() ? 'Following' : 'Follow'}
-                    </Button>
-                  )}
+              <Container
+                sx={{ display: 'flex', width: 'max-content', justifyContent: 'space-between' }}>
+                <Paper variant='outlined' sx={{ marginTop: '1rem', p: { xs: 2, md: 3 } }}>
+                  <Box sx={{ display: 'flex', justifyContent: ' space-around' }}>
+                    <Avatar
+                      alt='Remy Sharp'
+                      src={currentUserDetails?.avatar}
+                      sx={{ width: 100, height: 100 }}
+                    />
+                    <Box>
+                      <Typography
+                        sx={{ display: 'inline-block', padding: '1rem 1rem 0 1rem' }}
+                        variant='h5'
+                        component='h5'>
+                        {currentUserDetails?.firstName} {currentUserDetails?.lastName}
+                      </Typography>
+                      {user?.username === currentUserDetails?.username && (
+                        <Button
+                          onClick={handleOpen}
+                          sx={{ marginLeft: '8rem' }}
+                          variant='contained'>
+                          Edit
+                        </Button>
+                      )}
+                      {user.username !== currentUserDetails.username && (
+                        <Button variant='contained' h='2rem' onClick={unFollowOrFollowHandler}>
+                          {isFollowed() ? 'Following' : 'Follow'}
+                        </Button>
+                      )}
 
-                  <Typography sx={{ padding: '0 1rem 1rem 1rem' }} variant='p' component='p'>
-                    @{currentUserDetails?.username}
-                  </Typography>
-                  <Typography sx={{ padding: '0 1rem 0 1rem' }} variant='p' component='p'>
-                    {currentUserDetails?.bio}
-                  </Typography>
+                      <Typography sx={{ padding: '0 1rem 1rem 1rem' }} variant='p' component='p'>
+                        @{currentUserDetails?.username}
+                      </Typography>
+                      <Typography sx={{ padding: '0 1rem 0 1rem' }} variant='p' component='p'>
+                        {currentUserDetails?.bio}
+                      </Typography>
 
-                  <Link
-                    sx={{ display: 'flex', padding: '0 1rem 1rem 1rem' }}
-                    href={currentUserDetails?.website}
-                    target='_blank'
-                    rel='noopener'>
-                    {currentUserDetails?.website}
-                  </Link>
-                  <Typography sx={{ padding: '1rem' }} variant='span' component='span'>
-                    {postDetails.length} Post
-                  </Typography>
-                  <Typography
-                    onClick={handleOpenFollowersModal}
-                    sx={{ padding: '1rem', cursor: 'pointer' }}
-                    variant='span'
-                    component='span'>
-                    {currentUserDetails?.followers.length} Followers
-                  </Typography>
+                      <Link
+                        sx={{ display: 'flex', padding: '0 1rem 1rem 1rem' }}
+                        href={currentUserDetails?.website}
+                        target='_blank'
+                        rel='noopener'>
+                        {currentUserDetails?.website}
+                      </Link>
+                      <Typography sx={{ padding: '1rem' }} variant='span' component='span'>
+                        {postDetails.length} Post
+                      </Typography>
+                      <Typography
+                        onClick={handleOpenFollowersModal}
+                        sx={{ padding: '1rem', cursor: 'pointer' }}
+                        variant='span'
+                        component='span'>
+                        {currentUserDetails?.followers.length} Followers
+                      </Typography>
 
-                  <Typography
-                    onClick={handleOpenFollowingModal}
-                    sx={{ padding: '1rem', Scursor: 'pointer' }}
-                    variant='span'
-                    component='span'>
-                    {currentUserDetails?.following.length} following
-                  </Typography>
-                </Box>
-              </Box>
-            </Paper>
+                      <Typography
+                        onClick={handleOpenFollowingModal}
+                        sx={{ padding: '1rem', Scursor: 'pointer' }}
+                        variant='span'
+                        component='span'>
+                        {currentUserDetails?.following.length} following
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Paper>
+              </Container>
+
+              {postDetails?.length > 0 ? (
+                postDetails.map((post) => <PostFeed key={post._id} post={post} />)
+              ) : (
+                <Typography sx={{ textAlign: 'center' }} variant='h4' component='h4'>
+                  No Post Yet
+                </Typography>
+              )}
+            </ThemeProvider>
           </Container>
-
-          {postDetails?.length > 0 ? (
-            postDetails.map((post) => <PostFeed key={post._id} post={post} />)
-          ) : (
-            <Typography sx={{ textAlign: 'center' }} variant='h4' component='h4'>
-              No Post Yet
-            </Typography>
-          )}
-        </ThemeProvider>
+          <Suggestion />
+        </Box>
       )}
     </>
   );
